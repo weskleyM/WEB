532ae09e8d180bf5805e47d7e054cc7c272d5f46
weskleyM <weskley512@gmail.com>
2019-04-29 21:42:00 -0300

    WEB

---
+++ login/src/java/br/com/ufc/servlet/FiltroLogin.java
@@ -0,0 +1,41 @@
+package br.com.ufc.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+  
+@WebFilter("/*")
+public class FiltroLogin implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        HttpSession sessao = httpRequest.getSession();
+        if(sessao.getAttribute("usuarioLogado") != null || httpRequest.getRequestURI().endsWith("loginForm") || httpRequest.getRequestURI().endsWith("efetuarLogin") || httpRequest.getRequestURI().contains("resources")) {
+        chain.doFilter(request, response);
+        return;
+        }
+        httpResponse.sendRedirect("loginForm");
+    }
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+}
